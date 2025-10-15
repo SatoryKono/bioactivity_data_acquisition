7219b5855d45f4b7e3f6675923bfeff258d2e7a6
SatoryKono <821311@gmail.com>
2025-10-15 12:45:34 +0300

    1

---
+++ src/library/documents/__init__.py
@@ -0,0 +1,57 @@
+"""Document ETL pipeline components."""
+
+from __future__ import annotations
+
+from library.documents.config import (
+    ALLOWED_SOURCES,
+    ConfigLoadError,
+    DEFAULT_ENV_PREFIX,
+    DATE_TAG_FORMAT,
+    DocumentConfig,
+    DocumentHTTPGlobalSettings,
+    DocumentHTTPRetrySettings,
+    DocumentHTTPSettings,
+    DocumentIOSettings,
+    DocumentInputSettings,
+    DocumentOutputSettings,
+    DocumentRuntimeSettings,
+    SourceToggle,
+    load_document_config,
+)
+from library.documents.pipeline import (
+    DocumentETLResult,
+    DocumentHTTPError,
+    DocumentIOError,
+    DocumentPipelineError,
+    DocumentQCError,
+    DocumentValidationError,
+    read_document_input,
+    run_document_etl,
+    write_document_outputs,
+)
+
+__all__ = [
+    "ALLOWED_SOURCES",
+    "ConfigLoadError",
+    "DEFAULT_ENV_PREFIX",
+    "DATE_TAG_FORMAT",
+    "DocumentConfig",
+    "DocumentETLResult",
+    "DocumentHTTPError",
+    "DocumentHTTPGlobalSettings",
+    "DocumentHTTPRetrySettings",
+    "DocumentHTTPSettings",
+    "DocumentIOError",
+    "DocumentIOSettings",
+    "DocumentInputSettings",
+    "DocumentOutputSettings",
+    "DocumentPipelineError",
+    "DocumentQCError",
+    "DocumentRuntimeSettings",
+    "DocumentValidationError",
+    "SourceToggle",
+    "load_document_config",
+    "read_document_input",
+    "run_document_etl",
+    "write_document_outputs",
+]
