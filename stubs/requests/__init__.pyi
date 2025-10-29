eeabb3640fd5fba405b2feba9973e1777ec0de6a
SatoryKono <821311@gmail.com>
2025-10-29 10:25:59 +0300

    Add local requests stubs for mypy

--- stubs/requests/__init__.pyi
+++ stubs/requests/__init__.pyi
@@ -1,6 +1,10 @@
 from typing import Any, MutableMapping
+from types import TracebackType
+
 
 class Response:
+    """Minimal subset of ``requests.Response`` used by the project."""
+
     status_code: int
     headers: MutableMapping[str, str]
     text: str
@@ -11,19 +15,48 @@ class Response:
     def raise_for_status(self) -> None: ...
 
 
+class PreparedRequest:
+    method: str
+    url: str
+    headers: MutableMapping[str, str]
+    body: bytes | None
+
+
 class Session:
     headers: MutableMapping[str, str]
 
     def __init__(self) -> None: ...
+    def __enter__(self) -> "Session": ...
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc: BaseException | None,
+        tb: TracebackType | None,
+    ) -> None: ...
+
     def request(self, method: str, url: str, **kwargs: Any) -> Response: ...
     def get(self, url: str, **kwargs: Any) -> Response: ...
     def post(self, url: str, **kwargs: Any) -> Response: ...
     def close(self) -> None: ...
 
 
+def request(method: str, url: str, **kwargs: Any) -> Response: ...
 def get(url: str, **kwargs: Any) -> Response: ...
 def post(url: str, **kwargs: Any) -> Response: ...
 
-def Request(method: str, url: str, **kwargs: Any) -> Any: ...
+
+def Request(method: str, url: str, **kwargs: Any) -> PreparedRequest: ...
+
+
+__all__ = [
+    "PreparedRequest",
+    "Request",
+    "Response",
+    "Session",
+    "request",
+    "get",
+    "post",
+    "exceptions",
+]
 
 from . import exceptions as exceptions
