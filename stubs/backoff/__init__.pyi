4b94d6056f7151aff6d45c0b54cccd059ff0ba90
SatoryKono <821311@gmail.com>
2025-10-30 12:43:10 +0300

    1

---
+++ stubs/backoff/__init__.pyi
@@ -0,0 +1,20 @@
+from typing import Any, Callable, TypeVar, ParamSpec
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+def constant(interval: float) -> Any: ...
+
+def on_exception(
+    *,
+    wait_gen: Any,
+    interval: float | int | None = ...,  # accepts our usage with interval=0
+    exception: Any,
+    max_tries: int | None = ...,
+    giveup: Callable[[Exception], bool] | None = ...,
+    on_backoff: Callable[[dict[str, Any]], None] | None = ...,
+    on_giveup: Callable[[dict[str, Any]], None] | None = ...,
+    logger: Any | None = ...,
+) -> Callable[[Callable[P, T]], Callable[P, T]]: ...
+
+
