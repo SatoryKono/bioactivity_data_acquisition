9a491d65758c012d080ca438f6a992f1a0d1439b
SatoryKono <821311@gmail.com>
2025-11-07 21:10:12 +0300

    Delete src directory

--- src/library/cli/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Entry point for running CLI as module."""
-
-from . import main
-
-if __name__ == "__main__":
-    main()
--- src/library/schemas/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Data validation schemas for the ETL pipeline."""
-
-from .input_schema import RawBioactivitySchema
-from .output_schema import NormalizedBioactivitySchema
-
-__all__ = ["RawBioactivitySchema", "NormalizedBioactivitySchema"]
