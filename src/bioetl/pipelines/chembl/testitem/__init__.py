49c85f355f9ebf5a0a66e0143b957e69be943114
SatoryKono <821311@gmail.com>
2025-11-27 14:43:31 +0300

    1

--- src/bioetl/pipelines/chembl/testitem/__init__.py
+++ src/bioetl/pipelines/chembl/testitem/__init__.py
@@ -1,13 +1,4 @@
 from .run import TestItemChemblPipeline
-from .extract import run_extract
-from .transform import run_transform
-from .validate import run_validate
-from .write import run_write
-
 __all__ = [
     "TestItemChemblPipeline",
-    "run_extract",
-    "run_transform",
-    "run_validate",
-    "run_write",
 ]
